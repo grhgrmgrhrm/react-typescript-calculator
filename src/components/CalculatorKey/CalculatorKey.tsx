403fb5174750a198966f4c2282c98008ce3f6e34
grhgrmgrhrm <grhgrmgrhrm@gmail.com>
2021-11-18 21:50:40 +0300

    feat: add Calculator app to git

---
+++ src/components/CalculatorKey/CalculatorKey.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import './CalculatorKey.scss';
+
+interface ICalculatorKeyProps {
+  keyValue: string | number;
+  onClick: () => void;
+  className?: string;
+  disabled?: boolean;
+}
+
+export const CalculatorKey: FC<ICalculatorKeyProps> = ({ onClick, className, keyValue, disabled }) => (
+  <button className={`calculator-key ${className}`} type="button" onClick={onClick} disabled={disabled}>
+    {keyValue}
+  </button>
+);
